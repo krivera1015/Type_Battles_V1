fe4d1c850f1ddb67bf7b10beb8b209c335054764
Kevin RRivera <krivera1015@gmail.com>
2018-09-11 17:21:43 -0400

    Matching Algos Midway

--- Front_end_files/src/game.js
+++ Front_end_files/src/game.js
@@ -1,25 +1,41 @@
 const playPage = document.querySelector(".play-page")
 
 //get random function
-get_random = function (list) {
-  return list[Math.floor((Math.random()*list.length))];
+  get_random = function (list) {
+    return list[Math.floor((Math.random()*list.length))];
+  }
+
+  let snippet = ''
+
+  //getting snippets and appending to gamePage
+  const gamePage = () => {
+    GoogleAdapter.getBooks()
+    .then(obj => {
+      snippet = get_random(obj.items.map(book => {
+        return(book.searchInfo.textSnippet)
+      }))
+      playPage.innerHTML = `
+      <div id ="book-image">
+        <img src=book.volumeInfo.imageLinks.small>
+      </div>
+        <div id="snippet-box">
+        <p id="snippet">${snippet}</p>
+      </div>
+      <div id="textArea">
+        <textArea placeholder="Get, set ... TYPE"rows="8" cols="100"></textArea>
+      </div>
+      `
+  })
+    const textKey = document.body.onkeyup =
+    function comparingLetters(event){
+      if (event.target.tagName === 'TEXTAREA') {
+        console.log(snippet.match(event.target.value))
+      }
+    }
+
+    // event.target.tagName.style.backgroundColor = "red"
+
+
 }
 
-//getting snippets and appending to gamePage
-const gamePage = () => {
-  GoogleAdapter.getBooks()
-  .then(obj => {
-    const book = obj.items.map(book => {
-      return book.searchInfo.textSnippet
-    })
-    playPage.innerHTML = `
-    <div id ="book-image">
-      <img src=book.volumeInfo.imageLinks.small>
-    </div>
-    <div id="snippet">
-      <p>${get_random(book)}</p>
-    </div>
-    <textArea placeholder="Get, set ... TYPE"rows="8" cols="100"></textArea>
-    `
-  })
-}
+//getting snippet into letters that will be compared
