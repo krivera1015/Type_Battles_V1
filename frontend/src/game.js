dbbffbc1b5354b245522f6b3228134d1bb4d881c
Gabriel-Batista <gabedbatista@gmail.com>
2018-09-12 17:31:58 -0400

    working code

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -12,22 +12,16 @@ get_random = function (list) {
 //getting snippets and appending to gamePage
 function gamePage() {
     GoogleAdapter.getBooks()
-        .then(obj => {
-            let snippet = get_random(obj.items.map(book => (book.searchInfo.textSnippet)))
+    .then(json => {
+        let snippet = get_random(json.items.map(book => (book.searchInfo.textSnippet)))
 
-            playPage.innerHTML =
-                `
-                <div id ="book-image">
-                    <img src=book.volumeInfo.imageLinks.small>
-                </div>
-                    <div id="snippet-box">
-                    <p ><span id="green"></span>&nbsp;<span id="temp-green"></span><span id="temp-red"></span><span id="current-snippet">${snippet}</span></p>
-                </div>
-                <div id="textArea">
-                    <textArea placeholder="Get, set ... TYPE"rows="8" cols="100"></textArea>
-                </div>
-                `
-                return snippet
-        })
-        .then(snippet => {gameLogic(snippet)}) //starts event listeners and game logic
+        playPage.innerHTML =
+            `
+            <img id="book-image" src=book.volumeInfo.imageLinks.small>
+            <p id="snippet-box"><span id="green"></span><span id="temp-green"></span><span id="temp-red"></span><span id="current-snippet">${snippet}</span></p>
+            <textArea id="user-input" placeholder="Get, set ... TYPE"rows="8" cols="100"></textArea>
+            `
+            return snippet
+    })
+    .then(snippet => {gameLogic(snippet)}) //starts event listeners and game logic
 }
--- frontend/src/gameLogic.js
+++ frontend/src/gameLogic.js
@@ -1,6 +1,6 @@
 //The state of the board
-let focusedWord = 0
-let focusedChar = 0
+let focusedWord = ""
+let focusedChar = ""
 
 let splitSnippet = []
 
@@ -9,79 +9,125 @@ let greenWords = []
 let tmpGreen = []
 let tmpRed = []
 
-function gameLogic (snippet)  {
-  splitSnippet = snippet.split(" ")
 
-  //TODO: refactor this!
-  document.body.onkeyup = (event) => {
 
-      if (event.target.tagName === 'TEXTAREA' && alphanumericFilter()){
-          console.log(event.key)
-          if(document.querySelector("#temp-red").innerText !== ""){
-            charToTmpRed()
-          }
-          else if (event.code === "Space" && tmpGreen.join("") === splitSnippet[focusedWord])  {
-            drawGreenWord()
+function gameLogic(snippet) {
+    splitSnippet = snippet.split(" ")
+    focusedWord = splitSnippet[0]
+    focusedChar = focusedWord[0]
+    let userInput = document.querySelector("#user-input")
+
+    //TODO: refactor this!
+    userInput.onkeyup = (event) => {
+        if (event.target.tagName === 'TEXTAREA' && alphanumericFilter()) {
+            
+
+            console.log(userInput.value.slice(-1), focusedChar)
+
+            if (event.code === "Backspace") {
+                backspace()
+            }
+            else if (document.querySelector("#temp-red").innerText !== "") {
+                charToTmpRed()
+            }
+            else if (userInput.value.slice(-1) === " " && tmpGreen.join("") === focusedWord) {
+                drawGreenWord()
+                drawSnippet()
+            }
+            else if (userInput.value.slice(-1) === focusedChar) {
+                charToTmpGreen()
+            }
+            else {
+                charToTmpRed()
+            }
+        }
+    }
+
+
+
+    function drawSnippet() {
+        //Draw each word to the <p>
+        const completedSnippet = splitSnippet.join(" ")
+        const snippetContainer = document.querySelector("#current-snippet")
+        snippetContainer.innerText = completedSnippet
+    }
+
+    function drawGreenWord() {
+        //draw each word into <span> with green highlight style
+        // let completedWord = splitSnippet.shift()
+        const greenNode = document.querySelector("#green")
+        const tmpGreenNode = document.querySelector("#temp-green")
+        
+        greenWords.push(focusedWord + " ")
+        tmpGreenNode.innerText = ""
+        greenNode.innerText = greenWords.join(" ")
+        focusedWord = splitSnippet[0]
+        focusedChar = focusedWord[0]
+        tmpGreen = []
+        event.target.value = ""
+    }
+
+    function charToTmpGreen() {
+        //takes letter out of splitWord and puts it in <span> with green highlight style
+        const firstChar = focusedChar
+        const newWord = splitSnippet[0].substring(1)
+        const tmpGreenNode = document.querySelector("#temp-green")
+
+        tmpGreen.push(firstChar)
+        tmpGreenNode.innerText = tmpGreenNode.innerText + firstChar
+
+        if (newWord === "") {
+            splitSnippet.shift()
+        }
+        else {
+            splitSnippet[0] = newWord
+        }
+        
+        focusedChar = newWord[0]
+        drawSnippet()
+    }
+
+    function charToTmpRed() {
+        //draw each char into <span> with red highlight style
+        const firstChar = focusedChar
+        const newWord = splitSnippet[0].substring(1)
+        const tempRedNode = document.querySelector("#temp-red")
+        tempRedNode.innerText = tempRedNode.innerText + firstChar
+        tmpRed.push(firstChar)
+        splitSnippet[0] = newWord
+
+        focusedChar = newWord[0]
+        drawSnippet()
+    }
+
+    function backspace()    {
+        if (tmpRed.length === 0 && tmpGreen === 0)    {
+            return
+        }
+        else if (tmpRed.length === 0)   {
+            let fixedLetter = tmpGreen.pop()
+            const tempGreenNode = document.querySelector("#temp-green")
+
+            splitSnippet[0] = fixedLetter + splitSnippet[0]
+            tempGreenNode.innerText = tmpGreen.join("")
+            focusedWord = splitSnippet[0]
+            focusedChar = focusedWord[0]
             drawSnippet()
-          }
-          else if (event.key === splitSnippet[focusedWord][focusedChar] )   {
-              charToTmpGreen()
-          }
-          else{
-              charToTmpRed()
-          }
-      }
-  }
+        }
+        else    {
+            let fixedLetter = tmpRed.pop()
+            const tempRedNode = document.querySelector("#temp-red")
 
-
-
-  function drawSnippet()  {
-      //Draw each word to the <p>
-      const completedSnippet = splitSnippet.join(" ")
-      const snippetContainer = document.querySelector("#current-snippet")
-      snippetContainer.innerText = completedSnippet
-  }
-
-  function drawGreenWord()  {
-      //draw each word into <span> with green highlight style
-      let completedWord = splitSnippet.shift()
-      const greenNode = document.querySelector("#green")
-      const tmpGreenNode = document.querySelector("#temp-green")
-
-      greenWords.push(" " + completedWord)
-      tmpGreenNode.innerText = ""
-      greenNode.innerText = " " + tmpGreen.join()
-      event.target.value = ""
-  }
-
-  function charToTmpGreen()   {
-    //takes letter out of splitWord and puts it in <span> with green highlight style
-    const firstChar = splitSnippet[focusedWord][focusedChar]
-    const newWord = splitSnippet[focusedWord].substring(1)
-    const tempGreen = document.querySelector("#temp-green")
-    tmpGreen.push(firstChar)
-    tempGreen.innerText = tempGreen.innerText + firstChar
-    if (newWord === ""){
-      splitSnippet.shift()
+            splitSnippet[0] = fixedLetter + splitSnippet[0]
+            tempRedNode.innerText = tmpRed.join("")
+            focusedWord = splitSnippet[0]
+            focusedChar = focusedWord[0]
+            drawSnippet()
+        }
     }
-    else {
-      splitSnippet[0] = newWord
+
+    function alphanumericFilter() {
+        return (event.keyCode !== 20 && event.keyCode !== 16 && event.keyCode !== 17 && event.keyCode !== 18)
+
     }
-    drawSnippet()
-  }
-
-  function charToTmpRed()  {
-      //draw each char into <span> with red highlight style
-      const firstChar = splitSnippet[focusedWord][focusedChar]
-      const newWord = splitSnippet[focusedWord].substring(1)
-      const tempRed = document.querySelector("#temp-red")
-      tempRed.innerText = tempRed.innerText + firstChar
-      splitSnippet[0] = newWord
-      drawSnippet()
-  }
-
-  function alphanumericFilter() {
-    return (event.keyCode !== 20 && event.keyCode !== 16 && event.keyCode !== 17 && event.keyCode !== 18)
-
-  }
 }
