ed09643379bcd36e91bfe6d40a5d305df7cd93b7
Kevin RRivera <krivera1015@gmail.com>
2018-09-14 12:59:32 -0400

    Modal Done

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //login fields created
     const loginContainer = document.querySelector(".login-container")
     const playPage = document.querySelector(".play-page")
+    const playAgainDiv = document.querySelector(".play-again")
 
     loginContainer.innerHTML = `
     <form id="field-area">
@@ -25,16 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
             playPage.innerHTML = `<div class="modal fade" id="myModal" role="dialog">
                  <div class="modal-dialog modal-sm">
                    <div class="modal-content">
-                     <!-- <div class="modal-header">
-                       <button type="button" class="close" data-dismiss="modal">&times;</button>
-                     </div> -->
                      <h4 class="modal-title">Loading</h4>
                      <div class="modal-body">
                        <progress value="0" max="10" id="progressBar"></progress>
                      </div>
-                     <!-- <div class="modal-footer">
-                       <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                     </div> -->
                    </div>
                  </div>
                </div>`
@@ -44,6 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
                playPage.innerHTML = ""
                setTimeout(startTimer, 2000);
             gamePage()
+
         }
     })
 })
---
+++ frontend/src/playAgain.js
@@ -0,0 +1,26 @@
+// //words per minutes modal
+//
+// function playAgainDiv() {
+//   const playAgainDiv = document.querySelector(".play-again")
+//   console.log(play);
+//   playAgainDiv.innerHTML = `
+//     <div class="modal fade" id="endModal" role="dialog">
+//        <div class="modal-dialog modal-sm">
+//          <div class="modal-content">
+//            <div class="modal-header">
+//              <button type="button" class="close" data-dismiss="modal">&times;</button>
+//            </div>
+//            <h4 class="modal-title">Great job beast!</h4>
+//            <div class="modal-body">
+//              <p>Your WPM is ${}</p>
+//            </div>
+//            <div class="modal-footer">
+//              <button type="button" class="btn btn-default" data-again="modal">Play Again?</button>
+//              <button type="button" class="btn btn-default" data-dismiss="modal">Leave</button>
+//            </div>
+//          </div>
+//        </div>
+//      </div>`
+//      setTimeout(() => {$('#endModal').modal('show')}, 2000);
+//
+// }
